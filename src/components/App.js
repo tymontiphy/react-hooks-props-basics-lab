d87d0cc731dffc1192a0b0719ff80dd8a23b2636
Your Name <youremail@domain.com>
2024-11-06 23:21:41 +0300

    props react

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,16 @@ import Home from "./Home";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
+import user from "../data/user"
+// log the user data for debugging purposes
 console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links}/>
     </div>
   );
 }
